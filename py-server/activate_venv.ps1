d41f18fb3f1098a43df874f6acb86f697158ac69
Álvaro Caballero Laguna <127139062+ZiftK@users.noreply.github.com>
2025-02-12 14:54:48 -0600

    add: python venv activate package

--- py-server/activate_venv.ps1
+++ py-server/activate_venv.ps1
@@ -1,3 +1,8 @@
+param(
+    [switch]$fi
+)
+
+
 # Nombre del entorno virtual
 $venvName = "app_venv"
 
@@ -6,12 +11,17 @@ $venvPath = ".\$venvName"
 
 $exists = Test-Path $venvPath
 
+
 # Verificar si el entorno ya existe
 if ($exists) {
-    
+    Write-Host "$fi"
     # Activar el entorno virtual
     Write-Host "Activando el entorno virtual '$venvName'..."
     & "$venvPath\Scripts\Activate"
+
+    if ($fi){
+        pip install -r requirements.txt
+    }
     
 } else {
     Write-Host "Creando el entorno virtual '$venvName'..."
@@ -24,7 +34,7 @@ if ($exists) {
 
     # Instalar FastAPI y Uvicorn
     Write-Host "Instalando FastAPI y Uvicorn..."
-    pip install fastapi uvicorn
+    pip install -r requirements.txt
     
 
     Write-Host "Entorno virtual activado y dependencias instaladas."
