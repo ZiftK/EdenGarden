7366dcf65e635f8ac31bff5a390656d72cb04bb4
Yael <rayacova37@gmail.com>
2025-03-18 21:21:10 -0600

    feat: refactor HomePage component; update layout structure and styles for improved design

--- front_end/src/components/organisms/homePage.tsx
+++ front_end/src/components/organisms/homePage.tsx
@@ -7,8 +7,6 @@ export function HomePage(){
     return(
     <Box type='section'
         style={{
-            display: 'flex',
-            flexDirection: 'column-reverse',
             boxShadow: 'none',
             background: `
             linear-gradient(to right, #000e, #000a ,#0000),
@@ -23,44 +21,55 @@ export function HomePage(){
         }}
         >
     
-    <Box
-        type="div"
-        style={{
-            border: 'none',
-            marginTop: 5,
-            padding:0,
-            display: "flex",
-            gap: '10px',
-            backgroundColor: 'transparent',
-        }}
-    >
-        <BtnOutlined text="Nuestro servicio" style={{fontSize: 'var(--font-xs)'}}/>
-        <BtnFilled text="Contactanos" onClick={() => 1} $bg="var(--white-peristance-color)" col="#557C2B"/> 
-    </Box> 
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                height: '100%',
+                width: 'clamp(320px, 100%, 800px)',
+                justifySelf: 'center'
+            }}
+        >
+
+        <Box
+            type="div"
+            style={{
+                border: 'none',
+                marginTop: 5,
+                padding:0,
+                display: "flex",
+                gap: '10px',
+                backgroundColor: 'transparent',
+            }}
+        >
+            <BtnOutlined text="Nuestro servicio" style={{fontSize: 'var(--font-xs)'}}/>
+            <BtnFilled text="Contactanos" onClick={() => 1} $bg="var(--white-peristance-color)" col="#557C2B"/> 
+        </Box> 
 
     
-    {/* Number home page */}
-    <Typography 
-            children='+52 (55) 45034455'
-            variant='p'
-            style={{
-                color: "var(--white-peristance-color)"
-            }}
-    />
+        {/* Number home page */}
+        <Typography 
+                children='+52 (55) 45034455'
+                variant='p'
+                style={{
+                    color: "var(--white-peristance-color)"
+                }}
+        />
 
-    {/* Title Home Page */}
-    <Typography 
-            children='CREAMOS AMBIENTES NATURALES & ELEGANTES'
-            color='bg'
-            variant='h1'
-            style={{
-                fontSize: 'var(--font-lg)',
-                fontFamily:  "'Montserrat', sans-serif",
-                fontWeight: 'bold',
-                width: '300px',
-                color: "var(--white-peristance-color)"
-            }}
-            />
+        {/* Title Home Page */}
+        <Typography 
+                children='CREAMOS AMBIENTES NATURALES & ELEGANTES'
+                color='bg'
+                variant='h1'
+                style={{
+                    fontSize: 'var(--font-lg)',
+                    fontFamily:  "'Montserrat', sans-serif",
+                    fontWeight: 'bold',
+                    width: '300px',
+                    color: "var(--white-peristance-color)"
+                }}
+                />
+        </div>
     </Box>
-        )
+    )
 }
