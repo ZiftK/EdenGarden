a8c44c74a911d66a1c16fc2602f2eb8c098713d6
Álvaro Caballero Laguna <127139062+ZiftK@users.noreply.github.com>
2025-03-21 10:30:48 -0600

    refactor

--- src/backend/edensg_server/domain/entities/team.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-from employee import Employee
-
-class Team(BaseModel):
-    name: str
-    leader: Employee
-    employees: list[Employee]
