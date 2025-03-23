601ce8a4296fde141f6a1183d2f5616fa437d3ba
Álvaro Caballero Laguna <127139062+ZiftK@users.noreply.github.com>
2025-03-23 13:25:45 -0600

    update: first test passed

--- src/backend/test/use_cases/calendar_params.py
+++ src/backend/test/use_cases/calendar_params.py
@@ -1,29 +1,58 @@
-
+from backend.edensg_server.domain.entities.time_enums import EnumMonths
 
 get_working_days_params = [
     (
         {
         "initial_date": 
-            {"day": 1, "month": 1, "year": 2_000},
+            {"day": 1, "month": EnumMonths(1), "year": 2_000},
 
         "final_date": 
-            {"day": 30, "month": 3, "year": 2_000},
+            {"day": 30, "month": EnumMonths(3), "year": 2_000},
+        
+        "not_working_days": None,
         
         "current_sprint":
             {
-                "initial_date": {"day": 1, "month": 1, "year": 2_000},
+                "initial_date": {"day": 1, "month": EnumMonths(1), "year": 2_000},
                 
-                "final_date": {"day": 7, "month": 1, "year": 2_000},
+                "final_date": {"day": 7, "month": EnumMonths(1), "year": 2_000},
             }
         },
+
         [
-            {"day": 1, "month": 1, "year": 2_000},
-            {"day": 2, "month": 1, "year": 2_000},
-            {"day": 3, "month": 1, "year": 2_000},
-            {"day": 4, "month": 1, "year": 2_000},
-            {"day": 5, "month": 1, "year": 2_000},
-            {"day": 6, "month": 1, "year": 2_000},
-            {"day": 7, "month": 1, "year": 2_000},
+            {"day": 1, "month": EnumMonths(1), "year": 2_000},
+            {"day": 2, "month": EnumMonths(1), "year": 2_000},
+            {"day": 3, "month": EnumMonths(1), "year": 2_000},
+            {"day": 4, "month": EnumMonths(1), "year": 2_000},
+            {"day": 5, "month": EnumMonths(1), "year": 2_000},
+            {"day": 6, "month": EnumMonths(1), "year": 2_000},
+            {"day": 7, "month": EnumMonths(1), "year": 2_000},
         ]
-    )
+    ),
+    (
+        {
+        "initial_date": 
+            {"day": 1, "month": EnumMonths(1), "year": 2_000},
+
+        "final_date": 
+            {"day": 3, "month": EnumMonths(3), "year": 2_000},
+
+        "not_working_days": None,
+        
+        "current_sprint":
+            {
+                "initial_date": {"day": 30, "month": EnumMonths(1), "year": 2_000},
+                
+                "final_date": {"day": 3, "month": EnumMonths(2), "year": 2_000},
+            }
+        },
+
+        [
+            {"day": 30, "month": EnumMonths(1), "year": 2_000},
+            {"day": 31, "month": EnumMonths(1), "year": 2_000},
+            {"day": 1, "month": EnumMonths(2), "year": 2_000},
+            {"day": 2, "month": EnumMonths(2), "year": 2_000},
+            {"day": 3, "month": EnumMonths(2), "year": 2_000}
+        ]
+    ),
 ]
--- src/backend/test/use_cases/calendar_test.py
+++ src/backend/test/use_cases/calendar_test.py
@@ -1,6 +1,7 @@
 import pytest
-from src.backend.test.use_cases.calendar_params import get_working_days_params
-from backend.edensg_server.use_cases.calendar import get_working_days
+from backend.test.use_cases.calendar_params import get_working_days_params
+from backend.edensg_server.use_cases.calendar_calc import get_working_days_on_sprint
+from backend.edensg_server.domain.entities.project_calendar import Date, ProjectCalendar
 
 
 # from utils import load_module_from_path, py_server_path
@@ -11,10 +12,10 @@ from backend.edensg_server.use_cases.calendar import get_working_days
 
 # use_cases_module = load_module_from_path(use_cases_path)
 
-@pytest.mark.parametrize(
-    "params",
-    get_working_days_params
-)
-def test_get_working_days(params):
-    for _params, expected in params:
-        assert get_working_days(_params) == expected
+@pytest.mark.parametrize("params, expected", get_working_days_params)
+def test_get_working_days(params, expected):
+
+    params = ProjectCalendar(**params)
+    models: list[Date] = get_working_days_on_sprint(params)
+    models: list[dict] = [x.model_dump() for x in models]
+    assert models == expected
