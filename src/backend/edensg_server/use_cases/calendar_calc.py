b91340a4b988745f56e11e2cab30e55d99cb0ceb
Álvaro Caballero Laguna <127139062+ZiftK@users.noreply.github.com>
2025-03-24 13:22:36 -0600

    update: add substract_not_working_days function

--- src/backend/edensg_server/use_cases/calendar_calc.py
+++ src/backend/edensg_server/use_cases/calendar_calc.py
@@ -1,4 +1,4 @@
-from backend.edensg_server.domain.entities.project_calendar import ProjectCalendar, Date
+from backend.edensg_server.domain.entities.project_calendar import ProjectCalendar, Date, DateSchedule, DateTemplate, DayTemplate, ScheduleTemplates
 from datetime import date, timedelta
 from backend.edensg_server.domain.entities.time_enums import EnumDays
 
@@ -18,12 +18,50 @@ def convert_to_operational_date(date_entity: Date)-> date:
             date_entity.day
         )
 
+def substract_not_working_days(schedule_templates: ScheduleTemplates)->tuple[
+    set[DayTemplate],
+    set[DateTemplate],
+    set[EnumDays],
+    set[date]
+    ]:
+    """
+    Toma las plantillas de horario de un proyecto y las separa en
+    días laborables, fechas laborables, días no laborables y fechas no laborables.
+
+    :param schedule_templates: plantillas de horarios de proyecto
+
+    :returns:
+    Tupla con plantillas de días laborables, plantillas de fechas laborables, días no laborables, fechas no laborables; en ese orden.
+    """
+
+    day_templates: list[DayTemplate] = schedule_templates.by_day
+    date_templates: list[DateTemplate] = schedule_templates.by_date
+
+    if not date_templates:
+        date_templates: list[DateTemplate] = []
+    if not day_templates:
+        day_templates: list[DayTemplate] = []
+    
+    day_templates = set(day_templates)
+    date_templates = set(date_templates)
+
+    not_working_days = set(x for x in day_templates if not x.schedule.is_working_day)
+    not_working_dates = set(x for x in date_templates if not x.schedule.is_working_day)
+
+    day_templates -= not_working_days
+    date_templates -= not_working_dates
+
+    not_working_days = set(x.day.value -1 for x in not_working_days)
+    not_working_dates = set(convert_to_operational_date(x.date) for x in not_working_dates)
+
+    return day_templates, date_templates, not_working_days, not_working_dates
+
 def run_through_dates(*, 
     initial_date: date,
     final_date: date, 
     difference: timedelta, 
-    exclude_dates: list[date] = None,
-    exclude_days: list[EnumDays] = None
+    exclude_dates: set[date] = None,
+    exclude_days: set[EnumDays] = None
     )-> list[Date]:
     """
     Calcula todas las fechas desde `initial_date` hasta `final_date` con un salto de `difference` excluyendo
@@ -39,9 +77,6 @@ def run_through_dates(*,
     """
     auxiliar_date = initial_date
     between_dates: list[Date] = []
-    
-    exclude_dates: set = set(exclude_dates)
-    exclude_days: set = set(exclude_days)
 
     while not auxiliar_date == final_date + difference:
 
@@ -69,7 +104,18 @@ def run_through_dates(*,
 
     return between_dates
 
-def get_working_days_on_sprint(project_calendar: ProjectCalendar) -> list[Date]:
+def apply_schedule_templates(
+        working_days: list[Date], 
+        day_templates: list[DayTemplate], 
+        date_templates: list[DateTemplate])-> list[DateSchedule]:
+    
+    schedules: list[DateSchedule] = []
+    for wd in working_days:
+        new_date_schedule = DateSchedule(
+            date=wd,
+        )
+
+def get_working_days_on_sprint(project_calendar: ProjectCalendar) -> list[DateSchedule]:
     """
     Obtiene los dias laborables en un sprint de proyecto.
     """
@@ -79,22 +125,17 @@ def get_working_days_on_sprint(project_calendar: ProjectCalendar) -> list[Date]:
 
     templates = project_calendar.schedule_templates
 
-    exclude_dates = []
-    exclude_days = []
-    
-    day_templates = templates.by_day
-    date_templates = templates.by_date
-    
-    if day_templates:
-        exclude_days = [template.day.value - 1 for template in day_templates if not template.schedule.is_working_day]
-    if date_templates:
-        exclude_dates = [convert_to_operational_date(template.date) for template in date_templates if not template.schedule.is_working_day]
+    day_templates, date_templates, n_working_days, n_working_dates = substract_not_working_days(templates)
 
-    return run_through_dates(
+    working_days =  run_through_dates(
         initial_date=initial_date,
         final_date=final_date,
         difference=timedelta(days=1),
-        exclude_dates=exclude_dates,
-        exclude_days=exclude_days
+        exclude_dates=n_working_dates,
+        exclude_days=n_working_days
     )
+
+    return working_days
+
+    #TODO: ajustar cambias para devolver el nuevo tipo DateSchedule
 
