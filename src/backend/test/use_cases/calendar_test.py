c95c5be18bd2b13e15ef6e609193eb953d7bf323
Álvaro Caballero Laguna <127139062+ZiftK@users.noreply.github.com>
2025-04-02 09:07:13 -0600

    update: add apply_schedule_templates test

--- src/backend/test/use_cases/calendar_test.py
+++ src/backend/test/use_cases/calendar_test.py
@@ -2,14 +2,16 @@ import pytest
 from backend.edensg_server.use_cases.calendar_calc import\
     get_working_days_on_sprint,\
     run_through_dates,\
-    substract_not_working_days
+    substract_not_working_days,\
+    apply_schedule_templates
 
 from backend.edensg_server.domain.entities.project_calendar import Date, ProjectCalendar
 
 from backend.test.use_cases.calendar_params import\
     get_working_days_params,\
     run_throught_dates_params,\
-    substract_not_working_dates_params
+    substract_not_working_dates_params,\
+    apply_schedule_templates_params
 
 
 # @pytest.mark.parametrize("params, expected", get_working_days_params)
@@ -28,3 +30,7 @@ def test_run_throught_dates(params: dict, expected)-> None:
 @pytest.mark.parametrize("params, expected", substract_not_working_dates_params)
 def test_substract_working_days(params, expected)->None:
     assert substract_not_working_days(params) == expected
+
+@pytest.mark.parametrize("params, expected", apply_schedule_templates_params)
+def test_apply_schedule_templates(params, expected)->None:
+    assert apply_schedule_templates(*params) == expected
