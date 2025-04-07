ad64b17cfccc9365efed5bf22fe2870f8cb59501
Yael <rayacova37@gmail.com>
2025-04-06 21:27:14 -0600

    feat: migrate react components into next app

---
+++ src/next_front_end/src/_components/atoms/Flower Pots/DescriptionPots.tsx
@@ -0,0 +1,54 @@
+import { Box, Typography } from "@raul_yael/cleangui";
+import { BtnHref } from "../../moleculs/Button";
+
+export function DescriptionPots(){
+    return(
+        <Box type="div"
+            style={{
+                width:"100%",
+                border: "none",
+                boxShadow: "none",
+                background: "var(--green-200)",
+                margin: 0,
+                padding: "20px 40px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap:"20px"
+            }}
+        >
+            <Typography children="MACETAS" 
+                        style={{
+                            color: "var(--white-peristance-color)",
+                            fontSize: "var(--font-md)",
+                            justifySelf: "start",
+                        }}
+            />
+
+            <Typography children="En nuestra jardinería, sabemos que las macetas no solo sirven para cuidar tus plantas, sino también para embellecer tus espacios. Contamos con una amplia variedad de macetas en distintos materiales, tamaños y estilos, ideales para darle un toque natural y armonioso a tu hogar." 
+                        style={{
+                            color: "var(--white-peristance-color)",
+                            fontSize: "var(--font-xs)"
+                        }}
+            />
+
+            <Box type="div"
+                id="Btn_container"
+            >  
+
+                <BtnHref link="#" text="Ver más"
+                        weight={100}
+                        sxText="var(--font-xs)"
+                />
+                <BtnHref link="#" text="Compra una maceta" 
+                    weight={600} 
+                    sxText="var(--font-xs)"
+                    style={{
+                        color:"var(--green)",
+                    }}
+                    />
+            </Box> 
+        </Box>
+    )
+}
