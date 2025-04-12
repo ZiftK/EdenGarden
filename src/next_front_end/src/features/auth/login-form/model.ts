604eef80b2e646fe2b631c34ee3b493a6d4ed9a5
Yael <rayacova37@gmail.com>
2025-04-11 22:55:51 -0600

    feat: remove obsolete login form and model files, add empty index files for user and login features

--- src/next_front_end/src/features/auth/login-form/model.ts
+++
@@ -1,9 +0,0 @@
-import { endpoints } from "@/src/shared/api/endpoints"
-import { httpFunction } from "@/src/shared/api/httpClient";
-
-export async function loginUser(data: {exp: string, password: string}){
-    return httpFunction<{token: string}>(endpoints.login, {
-        method: 'POST',
-        body: JSON.stringify(data),
-    })
-}
