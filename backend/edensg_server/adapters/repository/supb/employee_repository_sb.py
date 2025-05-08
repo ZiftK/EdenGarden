9d4a233866fdb113e617eb381c502aae27a5e4be
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-08 10:42:17 -0600

    add: employee repository in supabase

---
+++ backend/edensg_server/adapters/repository/supb/employee_repository_sb.py
@@ -0,0 +1,52 @@
+from supabase import Client
+from backend.edensg_server.adapters.repository.supb.client import supabase_client
+from backend.edensg_server.domain.entities.employee import Employee
+from backend.edensg_server.adapters.repository.interface.employee_repository_interface import EmployeeRepository
+import asyncio
+
+class EmployeeRepositorySB(EmployeeRepository):
+    def __init__(self):
+        self.client = supabase_client
+        self.table = 'empleado'
+
+    def create_employee(self, data: Employee) -> int:
+        """Inserta un nuevo empleado en la base de datos."""
+        data_dict = data.model_dump(exclude={'id_empleado'})
+        response = self.client.table(self.table).insert(data_dict).execute()
+        return response.data[0]['id_empleado']
+
+    def get_all_employees(self) -> list[Employee]:
+        """Obtiene todos los empleados de la base de datos."""
+        response = self.client.table(self.table).select('*').execute()
+        return [Employee(**employee) for employee in response.data]
+
+    def find_employee_by_id(self, id: int) -> list[Employee]:
+        """Busca empleados por id."""
+        response = self.client.table(self.table).select('*').eq('id_empleado', id).execute()
+        return [Employee(**employee) for employee in response.data]
+
+    def find_employee_by_name(self, name: str) -> list[Employee]:
+        """Busca empleados por nombre."""
+        response = self.client.table(self.table).select('*').ilike('nombre', f'%{name}%').execute()
+        return [Employee(**employee) for employee in response.data]
+
+    def find_employee_by_email(self, email: str) -> list[Employee]:
+        """Busca empleados por email."""
+        response = self.client.table(self.table).select('*').eq('email', email).execute()
+        return [Employee(**employee) for employee in response.data]
+
+    def update_employee_data(self, id: int, data: Employee) -> None:
+        """Actualiza la información de un empleado."""
+        self.client.table(self.table).update(data.model_dump(exclude={'id_empleado'})).eq('id_empleado', id).execute()
+
+    def delete_employee_data(self, id: int) -> None:
+        """Elimina un empleado de la base de datos."""
+        self.client.table(self.table).delete().eq('id_empleado', id).execute()
+
+async def main():
+    repo = EmployeeRepositorySB()
+    repo.delete_employee_data(16)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
