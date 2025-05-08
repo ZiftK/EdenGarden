45c1e17472411300a1aadce239cff54533933409
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-08 10:41:47 -0600

    update: now optional values are None by default and the employee id is a default value

--- backend/edensg_server/domain/entities/employee.py
+++ backend/edensg_server/domain/entities/employee.py
@@ -3,14 +3,14 @@ from pydantic import BaseModel
 
 
 class Employee(BaseModel):
-    id_empleado: int
+    id_empleado: Optional[int] = None
     nombre: str
     direccion: str
     telefono: str
-    email: Optional[str]
+    email: Optional[str] = None
     fecha_contratacion: str
-    fecha_salida: Optional[str]
-    fecha_recontratacion: Optional[str]
+    fecha_salida: Optional[str] = None  
+    fecha_recontratacion: Optional[str] = None
     clave: str
     rol: str
     puesto: str
