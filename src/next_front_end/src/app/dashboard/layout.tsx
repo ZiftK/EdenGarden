68fa98e6ad2fe619141857d19deccd332e691792
Yael <rayacova37@gmail.com>
2025-05-09 13:28:09 -0600

    feat: reorder components in DashboardLayout for improved structure and readability

--- src/next_front_end/src/app/dashboard/layout.tsx
+++ src/next_front_end/src/app/dashboard/layout.tsx
@@ -25,11 +25,11 @@ export default async function DashboardLayout({
 			<body>
 				<HydrateZustandProvider user={user}>
 					<div className='grid grid-cols-1 grid-rows-[55px_100px_245px_1fr] md:grid-rows-[55px_100px_125_1fr] md:h-dvh md:max-w-[700px] md:mx-auto md:grid-cols-[1fr_300px] xl:max-w-[1536px] xl:grid-cols-[200px_1fr_250px] xl:grid-rows-[55px_250px_1fr]  min-h-full xl:pt-2 pt-20 px-2  gap-4 py-4'>
-						<ResponsiveMenu />
 						<InfoUser user={user} />
 						<Calendar />
 						<MeetsDay />
 						{children}
+						<ResponsiveMenu />
 					</div>
 				</HydrateZustandProvider>
 			</body>
