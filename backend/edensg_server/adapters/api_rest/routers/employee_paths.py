a044d175988b3376a071baff4e1934caaa5764ce
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-09 21:18:20 -0600

    fix endpoints

--- backend/edensg_server/adapters/api_rest/routers/employee_paths.py
+++ backend/edensg_server/adapters/api_rest/routers/employee_paths.py
@@ -1,23 +1,22 @@
 from fastapi import APIRouter
 from backend.edensg_server.domain.entities.employee import Employee
 from backend.edensg_server.adapters.repository.supb.employee_repository_sb import EmployeeRepositorySB
-router = APIRouter()
+router = APIRouter('employee')
 
 employee_repository = EmployeeRepositorySB()
 
-@router.get("/employees")
+@router.get("/all")
 async def get_employees():
     return employee_repository.find_all_employees()
 
-@router.get("/employees/{employee_id}")
+@router.get("/id/{employee_id}")
 async def get_employee(employee_id: int):
     return employee_repository.find_employee_by_id(employee_id)
 
-@router.post("/employees")
+@router.post("/create")
 async def create_employee(employee: Employee):
     return employee_repository.create_employee(employee)
 
-@router.put("/employees/{employee_id}")
+@router.put("/update/{employee_id}")
 async def update_employee(employee_id: int, employee: Employee):
     return employee_repository.update_employee(employee_id, employee)
-    
--- backend/edensg_server/adapters/api_rest/routers/team_paths.py
+++ backend/edensg_server/adapters/api_rest/routers/team_paths.py
@@ -1,35 +1,34 @@
 from fastapi import APIRouter
 from backend.edensg_server.domain.entities.team import Team
-from backend.edensg_server.use_cases.team_use_cases import TeamUseCases
 from backend.edensg_server.adapters.repository.supb.team_repository_sb import TeamRepositorySB
 
 router = APIRouter(prefix='/team')
-team_use_cases = TeamUseCases(TeamRepositorySB())
+team_repository = TeamRepositorySB()
 
 @router.post('/create')
 async def create_team(team: Team):
-    return await team_use_cases.create_team(team)
+    return team_repository.create_team(team)
 
 @router.get('/id/{team_id}')
 async def get_team_by_id(team_id: int):
-    return await team_use_cases.get_team_by_id(team_id)
+    return team_repository.find_team_by_id(team_id)
 
 @router.get('/name/{team_name}')
 async def get_team_by_name(team_name: str):
-    return await team_use_cases.get_team_by_name(team_name)
+    return team_repository.find_team_by_name(team_name)
 
 @router.put('/update/{team_id}')
 async def update_team(team_id: int, team: Team):
     print(team.model_dump())
-    return await team_use_cases.update_team(team_id, team)
+    return team_repository.update_team_data(team_id, team)
 
 @router.delete('/delete/{team_id}')
 async def delete_team(team_id: int):
-    return await team_use_cases.delete_team(team_id)
+    return team_repository.delete_team_data(team_id)
 
 @router.get('/all')
 async def get_all_teams():
-    return await team_use_cases.get_all_teams()
+    return team_repository.get_all_teams()
 
 
 
