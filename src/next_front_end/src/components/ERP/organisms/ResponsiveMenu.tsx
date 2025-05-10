2bfd80dae202fa5c793d416d4f2321d8c7eb26da
Yael <rayacova37@gmail.com>
2025-05-10 09:59:34 -0600

    refactor: improve code formatting and readability in ResponsiveMenu component

--- src/next_front_end/src/components/ERP/organisms/ResponsiveMenu.tsx
+++ src/next_front_end/src/components/ERP/organisms/ResponsiveMenu.tsx
@@ -1,20 +1,51 @@
-import { CertifiedIcon, GroupIcon, StoreIcon } from "../../landing/atoms/Icons/Icons"
-import DesktopMenu from "./DesktopMenu"
-import MobileMenu from "./MobileMenu"
+import {
+	CertifiedIcon,
+	GroupIcon,
+	StoreIcon,
+} from '../../landing/atoms/Icons/Icons'
+import DesktopMenu from './DesktopMenu'
+import MobileMenu from './MobileMenu'
 
 const menuItems = [
-    { label: "Inicio", link: "/dashboard", icon: StoreIcon({ color: "var(--white-peristance-color)", size: [1.25,1.25] })},
-    { label: "Proyectos", link: "/dashboard/proyectos", icon: GroupIcon({ color: "var(--white-peristance-color)", size: [1.25,1.25] })},
-    { label: "Equipos", link: "/dashboard/equipos", icon: CertifiedIcon({ color: "var(--white-peristance-color)", size: [1.25,1.25] }) },
-    { label: "Empleados", link: "/dashboard/empleados", icon: CertifiedIcon({ color: "var(--white-peristance-color)", size: [1.25,1.25] })},
+	{
+		label: 'Inicio',
+		link: '/dashboard',
+		icon: StoreIcon({
+			color: 'var(--white-peristance-color)',
+			size: [1.25, 1.25],
+		}),
+	},
+	{
+		label: 'Proyectos',
+		link: '/dashboard/proyectos',
+		icon: GroupIcon({
+			color: 'var(--white-peristance-color)',
+			size: [20, 20],
+		}),
+	},
+	{
+		label: 'Equipos',
+		link: '/dashboard/equipos',
+		icon: CertifiedIcon({
+			color: 'var(--white-peristance-color)',
+			size: [1.25, 1.25],
+		}),
+	},
+	{
+		label: 'Empleados',
+		link: '/dashboard/empleados',
+		icon: CertifiedIcon({
+			color: 'var(--white-peristance-color)',
+			size: [1.25, 1.25],
+		}),
+	},
 ]
 
 export default function ResponsiveMenu() {
-
-    return (
-        <>
-            <DesktopMenu menuItems={menuItems}/>
-            <MobileMenu menuItems={menuItems}/>
-        </>
-    )
+	return (
+		<>
+			<DesktopMenu menuItems={menuItems} />
+			<MobileMenu menuItems={menuItems} />
+		</>
+	)
 }
