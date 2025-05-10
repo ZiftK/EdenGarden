2a0fcb5816006e067e3e1b2b35926482deb31bab
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-09 21:05:56 -0600

    update: entities was updated to match with data base

--- backend/edensg_server/domain/entities/project.py
+++ backend/edensg_server/domain/entities/project.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel
 from .team import Team
+from typing import Optional
+from .client import Client
 from .project_calendar import ProjectCalendar
 
 
 class Project(BaseModel):
-    name: str
-    teams: list[Team]
-    calendar: ProjectCalendar
+    id_proyecto: Optional[int] = None
+    nombre: str
+    descripcion: Optional[str] = None
+    estado: str
+    costo: float
+    cliente: Client
+    equipo: Optional[Team] = None
+    calendario: ProjectCalendar
