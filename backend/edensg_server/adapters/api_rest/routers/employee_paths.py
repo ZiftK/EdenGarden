01b6da667735fa5eaa44d56179f9a1400a3b2ee5
Álvaro Caballero Laguna <127139062+ZiftK@users.noreply.github.com>
2025-05-12 15:36:06 -0600

    update: employee paths now use employee controller;

--- backend/edensg_server/adapters/api_rest/routers/employee_paths.py
+++ backend/edensg_server/adapters/api_rest/routers/employee_paths.py
@@ -1,21 +1,22 @@
 from fastapi import APIRouter
 from backend.edensg_server.domain.entities.employee import Employee
-from backend.edensg_server.adapters.repository.supb.employee_repository_sb import employee_sb_repository as employee_repository
+from backend.edensg_server.use_cases.employee_use_cases import EmployeeController
 router = APIRouter(prefix='/employee')
 
+employee_controller = EmployeeController()
 
 @router.get("/all")
 async def get_employees():
-    return employee_repository.find_all()
+    return employee_controller.get_all_employees()
 
 @router.get("/id/{employee_id}")
 async def get_employee(employee_id: int):
-    return employee_repository.find_employee_by_id(employee_id)
+    return employee_controller.get_employee_by_id(employee_id)
 
 @router.post("/create")
 async def create_employee(employee: Employee):
-    return employee_repository.create_employee(employee)
+    return employee_controller.create_employee(employee)
 
 @router.put("/update/{employee_id}")
 async def update_employee(employee_id: int, employee: Employee):
-    return employee_repository.update_employee(employee_id, employee)
+    return employee_controller.update_employee(employee_id, employee)
